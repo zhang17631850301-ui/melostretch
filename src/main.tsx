eb2c799aee442843bff541db252ac284a0de618b
zhang17631850301-ui <zhang17631850301@gmail.com>
2026-08-30 23:34:18 +0800

    增加页面白屏恢复保护

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './auth/AuthContext.tsx';
+import { AppErrorBoundary } from './components/AppErrorBoundary.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <AppErrorBoundary>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </AppErrorBoundary>
   </StrictMode>,
 );
